b526ce5a1bed69157ce859c68ef1c3b8a22f3788
dariop14-cell <dariopetasny@gmail.com>
2025-04-08 11:02:08 -0300

    Creacion de producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { ProductoComponent } from './Productos/producto/producto.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/inicio',pathMatch:'full'},
     {path: 'inicio',component:InicioComponent}, 
-    {path: 'contacto',component:ContactoComponent}
+    {path: 'contacto',component:ContactoComponent},
+    {path: 'producto',component:ProductoComponent}
 ];
