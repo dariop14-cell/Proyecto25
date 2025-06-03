762f1697e1f9d7ca5c74dcb9fd0faf6ab9de68cb
wendytati653 <wendyolaya653@gmail.com>
2025-06-03 11:09:19 -0300

    avances3dejunio

--- src/app/paginas/carrito/carrito.component.ts
+++ src/app/paginas/carrito/carrito.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Producto } from '../../model/producto.model';
 import { CarritoService } from '../../servicios/carrito.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,7 +15,7 @@ import { CarritoService } from '../../servicios/carrito.service';
 export class CarritoComponent implements OnInit{
   productosEnCarrito: {producto: Producto; cantidad : number}[] = []
 
-  constructor(private carritoService: CarritoService){}
+  constructor(private carritoService: CarritoService, private router : Router){}
 
   ngOnInit(): void {
     this.carritoService.carrito$.subscribe((productos) => {
@@ -39,10 +40,23 @@ export class CarritoComponent implements OnInit{
   vaciarCarrito(){
     this.carritoService.vaciarCarrito()
   }
-
+  /*
   realizarCompra(){
     alert('Compra Realizada')
     this.vaciarCarrito()
-  }
+  }*/
 
+  //Navega al formulario de compra
+  irAFormularioCompra(){
+    //Redirige al usuario a la ruta '/compra', donde se encuentra el formulario 
+    this.router.navigate(['/compra']) 
+  }
+  
+  //Calcular el total del carrito de compras
+  calcularTotal():number{
+    //Recorre eñ arreglo de productos en el carrito y suma el resultado de (precio * cantidad) de cada item
+    return this.productosEnCarrito.reduce((total,item) => {
+      return total + item.producto.precio * item.cantidad
+    },0) //el acumulador 'total' comienza en 0
+  }
 }
--- src/app/servicios/carrito.service.ts
+++ src/app/servicios/carrito.service.ts
@@ -6,27 +6,56 @@ import { Producto } from '../model/producto.model';
   providedIn: 'root'
 })
 export class CarritoService {
-  private carritoSubject = new BehaviorSubject<{producto: Producto; cantidad: number}[]>([]);
-  carrito$= this.carritoSubject.asObservable()
-  
-  agregarAlcarrito(producto:Producto){
+  private carritoSubject = new BehaviorSubject<{ producto: Producto; cantidad: number }[]>([]);
+  carrito$ = this.carritoSubject.asObservable()
+
+  agregarAlcarrito(producto: Producto) {
     const productos = this.carritoSubject.getValue();
     const encontrado = productos.find(p => p.producto.id === producto.id)
 
-    if(encontrado){
+    if (encontrado) {
       encontrado.cantidad++
-    } else{
-      this.carritoSubject.next ([...productos, {producto, cantidad :1}]) 
+    } else {
+      this.carritoSubject.next([...productos, { producto, cantidad: 1 }])
 
     }
   }
 
-  eliminarDelCarrito(productoId: number){
+  eliminarDelCarrito(productoId: number) {
     const productos = this.carritoSubject.getValue().filter(p => p.producto.id !== productoId)
     this.carritoSubject.next(productos)
   }
-  vaciarCarrito(){
+  vaciarCarrito() {
     this.carritoSubject.next([])
   }
+
+  //Metodo para actualizar la cantidad de un producto en el carrito
+  actualizarCantidad(productoID: number, nuevaCantidad: number) {
+    //recorremos el carrito y actualizamos la cantidad del producto con el ID dado
+    const productos = this.carritoSubject.getValue().map(item => {
+      if (item.producto.id === productoID)
+        //Retornamos una copia del producto con la nueva cantidad
+        return {
+          ...item, cantidad: nuevaCantidad
+        }
+      return item
+    })
+    //Emitimos el nuevo estado del carrito 
+    this.carritoSubject.next(productos)
+  }
+
+  //Metodo para obtener los productos del carrito como un arreglo 
+  obtenerProductos(): {producto: Producto; cantidad: number}[]{
+    return this.carritoSubject.getValue();
+  }
+
+  //Metodo para calcular el total a pagar (precio * cantidad de cada producto)
+  obtenerTotal():number{
+    const productos = this.carritoSubject.getValue();
+    //Lo usamos reduce para sumar los subtotales de cada producto
+    return productos.reduce((total, item) => total + item.producto.precio * item.cantidad,0)
+
+  }
+
   constructor() { }
 }
